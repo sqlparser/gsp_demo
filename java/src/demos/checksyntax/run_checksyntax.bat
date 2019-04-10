d8d81a7bd5f28cd2539572a4c4d7c39c24ebbd10
takolee <tako.lee@foxmail.com>
2019-04-10 09:36:23 +0800

    add build and run script for checksyntax

---
+++ java/src/demos/checksyntax/run_checksyntax.bat
@@ -0,0 +1,36 @@
+echo off
+
+REM # This script/batch file run the gsp demo. 
+REM # You need to set the JAVA_HOME environment variable in 
+REM # the bin/setenv.bat file 
+REM # before running this script/batch file
+
+REM # Change directory to gsp Demo Home directory
+cd ..\..\..
+
+REM # Run the setenv to set the environment variables.
+call bin\setenv.bat
+
+   
+    if NOT EXIST %JAVA_CMD% (
+    echo. 
+    echo ***************************
+    echo JAVA_HOME is not set in the bin\setenv.bat
+    echo Please set the JAVA_HOME. 
+    echo eg. JAVA_HOME=C:\Program Files\Java\jdk1.7.0_80
+    echo ***************************
+    echo.
+    cd src\demos\checksyntax
+    pause
+    goto END
+    )
+
+REM # Run the gsp demo
+%JAVA_CMD% -cp %CLASSPATH% demos.checksyntax.checksyntax
+
+REM # Change back to the original directory
+cd src\demos\checksyntax
+
+pause
+
+:END
