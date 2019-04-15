c0af32831b05ccf1a9e53a99307ffa846e9f68d3
cnfree <cnfree2000@hotmail.com>
2019-04-15 09:28:43 +0800

    Need a script to build and run all Java demos #4

--- java/src/demos/analyzesp/sybase/compile_analyzesp.bat
+++ java/src/demos/analyzesp/sybase/compile_analyzesp.bat
@@ -8,7 +8,7 @@ REM # Set the target directory where the compiled class should be copied.
 set targetdir=build
 
 REM # Change directory to gsp Demo Home directory
-cd ..\..\..
+cd ..\..\..\..
 
 REM # Run the setenv to set the environment variables.
 call setenv\setenv.bat
@@ -22,18 +22,22 @@ call setenv\setenv.bat
     echo eg. JAVA_HOME=C:\Program Files\Java\jdk1.7.0_80
     echo ***************************
     echo.
-    cd src\demos\checksyntax
+    cd src\demos\analyzesp\sybase
     pause
     goto END
     )
 
+	if NOT exist %targetdir% (
+	    md %targetdir%
+	)
+
 REM # Compile the gsp demo
-%JAVAC_CMD% -d %targetdir% -classpath %CLASSPATH% src\demos\checksyntax\checksyntax.java
+%JAVAC_CMD% -d %targetdir% -classpath %CLASSPATH% src\demos\analyzesp\sybase\Analyze_SP.java
 
 echo Completed.
 
 REM # Change directory to the original directory
-cd src\demos\checksyntax
+cd src\demos\analyzesp\sybase
 
 pause
 
